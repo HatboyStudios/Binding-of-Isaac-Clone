f03b21492b3309362929f830f0ee30cb7ae4d7a8
Matthew Gray <matthew.riley.gray@outlook.com>
2025-08-08 12:25:34 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var test_variable;
+
+function setup() {
+  let screen = createCanvas(400, 400);
+
+  test_variable = new Sprite(200, 200, 50, 50);
+}
+
+function update() {
+  background("purple")
+}
